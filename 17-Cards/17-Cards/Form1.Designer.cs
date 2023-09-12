80316bbb037938b57b95cc966d3c6d2c99234100
athebyme <116726182+emybehta@users.noreply.github.com>
2023-09-12 14:11:35 +0300

    Task 17 "Cards"

---
+++ 17-Cards/17-Cards/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace _17_Cards
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pnl1 = new System.Windows.Forms.Panel();
+            this.btnSpawn = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblRez = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // pnl1
+            // 
+            this.pnl1.BackColor = System.Drawing.Color.Bisque;
+            this.pnl1.Location = new System.Drawing.Point(12, 12);
+            this.pnl1.Name = "pnl1";
+            this.pnl1.Size = new System.Drawing.Size(650, 450);
+            this.pnl1.TabIndex = 0;
+            // 
+            // btnSpawn
+            // 
+            this.btnSpawn.Location = new System.Drawing.Point(683, 12);
+            this.btnSpawn.Name = "btnSpawn";
+            this.btnSpawn.Size = new System.Drawing.Size(105, 51);
+            this.btnSpawn.TabIndex = 0;
+            this.btnSpawn.Text = "Spawn";
+            this.btnSpawn.UseVisualStyleBackColor = true;
+            this.btnSpawn.Click += new System.EventHandler(this.BtnSpawn_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(683, 231);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(213, 381);
+            this.listBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(701, 97);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // lblRez
+            // 
+            this.lblRez.AutoSize = true;
+            this.lblRez.Location = new System.Drawing.Point(12, 482);
+            this.lblRez.Name = "lblRez";
+            this.lblRez.Size = new System.Drawing.Size(0, 13);
+            this.lblRez.TabIndex = 3;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 520);
+            this.Controls.Add(this.lblRez);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnSpawn);
+            this.Controls.Add(this.pnl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnl1;
+        private System.Windows.Forms.Button btnSpawn;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblRez;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
